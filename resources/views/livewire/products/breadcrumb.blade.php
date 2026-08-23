c2727a22bcc15e31f4a8bf27230893ec89c15d24
Md Monirul Islam Taluqdar <82707883+monirulislam0@users.noreply.github.com>
2026-08-23 23:23:03 +0600

    Updated

--- resources/views/livewire/products/breadcrumb.blade.php
+++ resources/views/livewire/products/breadcrumb.blade.php
@@ -1,8 +1,8 @@
 <nav aria-label="breadcrumb">
     <ol class="breadcrumb">
-        <li class="breadcrumb-item"><a href="{{ route('frontend.home') }}">Home</a></li>
+        <li class="breadcrumb-item" style="color: #000 !important;  "><a style="color: #000 !important; href="{{ route('frontend.home') }}">Home</a></li>
 
-        <li class="breadcrumb-item"><a href="{{ route('frontend.product.center') }}">Products Center</a></li>
-        <li class="breadcrumb-item active" aria-current="page">{{ $category_name }}</li>
+        <li class="breadcrumb-item" style="color: #000 !important;"><a style="color: #000 !important; href="{{ route('frontend.product.center') }}">Products Center</a></li>
+        <li class="breadcrumb-item active" aria-current="page" style="color: #000 !important;">{{ $category_name }}</li>
     </ol>
 </nav>
